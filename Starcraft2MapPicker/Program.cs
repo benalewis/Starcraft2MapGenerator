be9c5847cbe0c26d649dbd83f619fa0d206c1bc5
Ben Lewis <benalewis@hotmail.co.uk>
2018-11-29 23:34:02 +0000

    init

---
+++ Starcraft2MapPicker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Starcraft2MapPicker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var maps = new List<string>()
+            {
+                "Automaton LE",
+                "Blueshift LE",
+                "Cerulean Fall LE",
+                "Kairos Junction LE",
+                "Para Site LE",
+                "Port Aleksander LE",
+                "Stasis LE"
+            };
+
+            var rand = new Random();
+
+            var random = rand.Next(0, maps.Count);
+
+            Console.WriteLine(maps[random]);
+        }
+    }
+}
